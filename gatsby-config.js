54e93f2470e2bf56e77b19062f70129e24d8736e
Franco Ramirez <francoramirezreyes@gmail.com>
2021-04-25 04:28:16 -0500

    rss test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
+const title = "Fredrare.com"
+const pathPrefix = '/posts'
+
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Blog Demo",
+    title: title,
   },
   plugins: [
     'gatsby-plugin-postcss',
@@ -12,6 +15,55 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
         environment: process.env.DATO_ENVIRONMENT,
       },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site: allDatoCmsSite(limit: 1) {
+              edges {
+                node {
+                  id
+                }
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            query: `
+              {
+                posts: allDatoCmsPost {
+                  edges {
+                    node {
+                      slug
+                      title
+                      excerpt
+                      author {
+                        name
+                      }
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            serialize: function({query: { posts }}) {
+              console.log(JSON.stringify(posts))
+              return posts.edges.map(({ node }) => ({
+                description: node.excerpt.trim(),
+                author: node.author.name.trim(),
+                date: node.date.trim(),
+                title: node.title.trim(),
+                url: `${process.env.HOSTNAME}${pathPrefix}/${node.slug}`
+              }))
+            },
+            title: title,
+            output: 'feed.rss',
+          }
+        ]
+      }
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
