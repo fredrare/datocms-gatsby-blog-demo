26e3e049ae482373f8cd13c3f828505abc2c8ff0
Franco Ramirez <francoramirezreyes@gmail.com>
2021-04-27 01:34:41 -0500

    replaced nav with footer and indented the block

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,32 +6,32 @@ import { siteName } from '../common/strings'
 
 export default function Footer({ socialMedia }) {
   return (
-<nav className="justify-between px-3 py-5 bg-gray-100">
-  <div className="px-4 mx-auto flex flex-wrap items-center justify-between content-center">
-    <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
-      <Link to="/" title={siteName}>
-        { siteName }
-      </Link>
-      {' (Derechos reservados - 2021)'}
-    </div>
-    <div className="items-center flex flex-row">
-      <ul className="flex flex-row list-none ml-auto">
-        {
-          socialMedia.map( platformImage => (
-              <li className="flex items-center">
-                <a
-                  href={platformImage.customData.link}
-                  title={platformImage.title}
-                  className="px-3 py-2 hover:opacity-75">
-                  <Image className="content-center" fixed={platformImage.fixed} alt={platformImage.alt}/>
-                </a>
-              </li>
-            )
-          )
-        }
-      </ul>
-    </div>
-  </div>
-</nav>
+    <footer className="justify-between px-3 py-5 bg-gray-100">
+      <div className="px-4 mx-auto flex flex-wrap items-center justify-between content-center">
+        <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+          <Link to="/" title={siteName}>
+            { siteName }
+          </Link>
+          {' (Derechos reservados - 2021)'}
+        </div>
+        <div className="items-center flex flex-row">
+          <ul className="flex flex-row list-none ml-auto">
+            {
+              socialMedia.map( platformImage => (
+                  <li className="flex items-center">
+                    <a
+                      href={platformImage.customData.link}
+                      title={platformImage.title}
+                      className="px-3 py-2 hover:opacity-75">
+                      <Image className="content-center" fixed={platformImage.fixed} alt={platformImage.alt}/>
+                    </a>
+                  </li>
+                )
+              )
+            }
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
