0b7d31f1f757f344424fae12fdc405fc3f2de162
Franco Ramirez <francoramirezreyes@gmail.com>
2021-07-27 19:58:43 -0500

    Fixed reading bar progress

--- src/components/reading-progress-bar.js
+++ src/components/reading-progress-bar.js
@@ -4,38 +4,32 @@ import { useEffect, useState } from 'react';
 export default function ReadingProgress ({ target }) {
     const [readingProgress, setReadingProgress] = useState(0);
     const scrollListener = () => {
-      if (!target.current) return
-  
-      const element         = target.current;
-      const totalHeight     = element.clientHeight - element.offsetTop - (window.innerHeight);
-      const windowScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  
-      if (windowScrollTop === 0) {
-        return setReadingProgress(0);
-      }
-  
-      if (windowScrollTop > totalHeight) {
-        return setReadingProgress(100);
-      }
-  
-      setReadingProgress((windowScrollTop / totalHeight) * 100);
+        if (!target.current) return
+
+        const element = target.current
+        const totalHeight = element.clientHeight - element.offsetTop - (window.innerHeight) + 230
+        const windowScrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+
+        if (windowScrollTop === 0) return setReadingProgress(0)
+
+        if (windowScrollTop > totalHeight) return setReadingProgress(100)
+
+        setReadingProgress((windowScrollTop / totalHeight) * 100)
     };
     
     useEffect(() => {
-      window.addEventListener("scroll", scrollListener);
-      return () => window.removeEventListener("scroll", scrollListener);
-    });
+        window.addEventListener("scroll", scrollListener)
+        return () => window.removeEventListener("scroll", scrollListener)
+    })
     
-    return (
-        <div
-            className="bg-pink-500 rounded sticky"
-            style={{
-                width: `${readingProgress}%`,
-                height: '0.3em',
-                top: 0,
-                zIndex:1000
-            }}
-        />
-    );
-  };
+    return <div
+        className={`bg-pink-500 ${readingProgress < 100 ? 'rounded ' : ''}rounded-l-none sticky`}
+        style={{
+            width: `${readingProgress}%`,
+            height: '0.3em',
+            top: 0,
+            zIndex:1000
+        }}
+    />
+}
 
