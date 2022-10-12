a1c8e01692ef8717028ea19479d21ec2e0509745
Franco Ramirez <francoramirezreyes@gmail.com>
2022-10-12 18:43:53 -0500

    🔧 CORS bypass via allorigins for comments

--- src/hooks/useFeedback.js
+++ src/hooks/useFeedback.js
@@ -5,8 +5,11 @@ const useFeedback = commentSource => {
   const [reactionNumber, setReactionNumber] = useState(0)
   useEffect(() => {
     fetch(
-      "https://giscus.app/api/discussions?repo=fredrare/blog-interactions&category=Announcements&number=0&strict=false&term=" +
-        encodeURIComponent(commentSource)
+      "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent(
+          "https://giscus.app/api/discussions?repo=fredrare/blog-interactions&category=Announcements&number=0&strict=false&term=" +
+            commentSource
+        )
     )
       .then(resp => resp.json())
       .then(data => {
